cce53f8349a16ed0bb740b386454a725f8e689aa
nedinfra <111425772+nedinfra@users.noreply.github.com>
2023-01-18 10:20:54 +0100

    bs-tab-group

--- src/app/tabs/bs-tab-group/bs-tab-group.component.ts
+++ src/app/tabs/bs-tab-group/bs-tab-group.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
+import { TabGroupComponent } from '../tab-group/tab-group.component';
+import { forwardRef } from '@angular/core';
+
+const BsTabGroupProvider = {
+  provide: TabGroupComponent,
+  useExisting: forwardRef(() => BsTabGroupComponent),
+};
 
 @Component({
   selector: 'app-bs-tab-group',
   templateUrl: './bs-tab-group.component.html',
   styleUrls: ['./bs-tab-group.component.css'],
-  standalone: true
+  standalone: true,
+  providers: [BsTabGroupProvider],
+  imports: [NgFor, NgIf, NgTemplateOutlet],
 })
-export class BsTabGroupComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class BsTabGroupComponent extends TabGroupComponent {}
